f5dd8ba0e6f4cf48d190a8bec99b2762025a658a
Sofia Villar <sofvillar@gmail.com>
2018-08-30 13:15:21 +0200

    Added references for loop

--- randomizeAnswers.pl
+++ randomizeAnswers.pl
@@ -5,49 +5,52 @@ use Data::Dumper qw(Dumper);
 use List::Util qw(shuffle);
 
 
-my %answermaster = (
-	"Introduction to Perl for Programmers" => 1,
-	"Introduction to Perl for Programmers and Other Crazy People" => 0,
-	"Introduction to Programming for Pearlers" => 0,
-	"Introduction to Aussies for Europeans" => 0,
-	"Introduction to Python for Slytherins" => 0,
-);
-
-my %answerstudent = (
-	"Introduction to Perl for Programmers" => 0,
-	"Introduction to Perl for Programmers and Other Crazy People" => 0,
-	"Introduction to Programming for Pearlers" => 1,
-	"Introduction to Aussies for Europeans" => 0,
-	"Introduction to Python for Slytherins" => 0,
-
-);
-
+# Sum of points a students gets 
+# 1 point per correct answer
 my $points = 0;
 
-foreach my $q (keys %studentquestions){
-	if(%studentquestions{$q} eq %masterquestions{$q}){
-		# Check if question was answered correctly
+# Interate through the master file and look for the question in the student's exam file
+foreach my $q (keys %$masterExam){
+	if(%$studentExam{$q} eq %$masterExam{$q}){
+		
+		# Variable used to check if question was answered correctly
 		my $counter = 0;
 		
-		foreach my $ans (keys %answerstudent){
-			$counter += $answerstudent{$ans};
+		# Count the values of student's exam - helps determine if and how answered
+		foreach my $ans (keys %{%$studentExam{$q}}){
+			$counter += %{%$studentExam{%$q{$ans}}};
 		}
 		
+		# Case that question wasn't answered
 		if($counter < 1){
+		
 			print "Incorrect: Student did not answer question.";
+			
+		# Case more than one answer ticked
 		} elsif($counter > 1){
+		
 			print "Incorrect: Too many answers.";
+			
 		} else{
-			foreach my $ans (keys %answerstudent){
-				if($answerstudent{$ans} ne $answermaster{$ans}){
+		# Check if checked answer is correct answer
+		
+			foreach my $ans (keys %{%$masterExam{$q}}){
+			
+				if(%{%$studentExam{$q{$ans}} ne %{%$masterExam{%$q{$ans}}}){
 					$counter = 0;
 				}
 			}
+			
+			#Check if result of correction
 			if($counter){
+				# Add points 
 				$points++;
 				print "Correct."
+				
 			}else{
+			
 				print "Incorrect: Wrong answer."
+				
 			}
 		}
 	}
