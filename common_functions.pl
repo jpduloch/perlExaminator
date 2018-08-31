8602436e750240d778a302a2677380bbdf8e913e
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2018-08-31 14:26:54 +0200

    part2 extention.

--- common_functions.pl
+++ common_functions.pl
@@ -2,6 +2,64 @@ use v5.28;
 use strict;
 use diagnostics;
 
+# Build Stopwordlist-array
+  my @stopWordList;
+
+  open (my $fh, '<', "stop-word-list.txt");
+  chomp(@stopWordList = <$fh>);
+  close $fh;
+
+sub compare_twounevenstrrings{
+  my ($originalString, $comparisonString) = @_;
+
+  # Lowercase
+  $originalString = lc $originalString;
+  $comparisonString = lc $comparisonString;
+
+  # removing any “stop words” from the text
+  # Source: https://stackoverflow.com/questions/26820340/perl-remove-stopwords-from-string
+  my ($rx) = map qr/(?:$_)/, join "|", map qr/\b\Q$_\E\b/, @stopWordList;
+
+  $originalString =~ s/$rx//g;
+  $comparisonString =~ s/$rx//g;
+
+  #removing any sequence of whitespace characters at the start and/or the end of the text
+  $originalString =~ s/^\s+|\s+$//g;
+  $comparisonString =~ s/^\s+|\s+$//g;
+
+  #replacing any remaining sequence of whitespace characters within the text with a single space character.
+  $originalString =~ s/\s+/ /g;
+  $comparisonString =~ s/\s+/ /g;
+
+  return (100/length $originalString) * levenshtein($originalString, $comparisonString);
+
+}
+
+use List::Util qw(min);
+
+sub levenshtein {
+  # Source: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Perl
+    my ($str1, $str2) = @_;
+    my @ar1 = split //, $str1;
+    my @ar2 = split //, $str2;
+
+    my @dist;
+    $dist[$_][0] = $_ foreach (0 .. @ar1);
+    $dist[0][$_] = $_ foreach (0 .. @ar2);
+
+    foreach my $i (1 .. @ar1){
+        foreach my $j (1 .. @ar2){
+            my $cost = $ar1[$i - 1] eq $ar2[$j - 1] ? 0 : 1;
+            $dist[$i][$j] = min(
+                        $dist[$i - 1][$j] + 1,
+                        $dist[$i][$j - 1] + 1,
+                        $dist[$i - 1][$j - 1] + $cost );
+        }
+    }
+
+    return $dist[@ar1][@ar2];
+}
+
 sub create_hashstructure_fromfile {
   # Subroutine reads the questions and answers from a file.
   # Returns header as string and a hash with
--- loaddatshittyfiles.pl
+++ loaddatshittyfiles.pl
@@ -7,23 +7,23 @@ use Data::Dumper qw(Dumper);
 use File::Find::Rule;
 use POSIX qw(strftime);
 use File::Spec;
-use List::Util qw(sum);
+use List::Util qw(sum all any);
 use Sort::Naturally qw(nsort);
 
 require '.\common_functions.pl';
 
 # Read MasterFiles
-  my ($masterfilepath, @responseFilesInput) = @ARGV;
-  my ($mastaHeader, $masterExam) = create_hashstructure_fromfile($masterfilepath);
+my ($masterfilepath, @responseFilesInput) = @ARGV;
+my ($mastaHeader, $masterExam) = create_hashstructure_fromfile($masterfilepath);
 
-  my @responseFiles;
+my @responseFiles;
 
-  for my $responseFileInput (@responseFilesInput){
-      my ($volume,$directories,$fileName) =  File::Spec->splitpath($responseFileInput);
-      push @responseFiles, File::Find::Rule->file()
-                                  ->name("$fileName")
-                                  ->in($directories);
-  }
+for my $responseFileInput (@responseFilesInput){
+    my ($volume,$directories,$fileName) =  File::Spec->splitpath($responseFileInput);
+    push @responseFiles, File::Find::Rule->file()
+                                ->name("$fileName")
+                                ->in($directories);
+}
 
 # Marking the exams
 
@@ -34,41 +34,90 @@ foreach my $studentExamFilePath (@responseFiles){
   my ($studentHeader, $studentExam) = create_hashstructure_fromfile($studentExamFilePath);
   my @studentExamMarks;
 
-  # Interate through the master file and look for the question in the student's exam file
-  foreach my $q (nsort keys %$masterExam){
+  # Iterate through the master file and look for the question in the student's exam file
+  foreach my $q (nsort keys %$masterExam) {
     my $point = "-";
+    my $studentQuestion;
 
-  	if(exists $studentExam->{$q}){
-
-        # Check if question is answered
-        if ((sum values  %{$studentExam->{$q}}) > 0) {
-
-          # Check if checked answer is correct answer
-          $point = 1;
-    			foreach my $ans (keys %{$masterExam->{$q}}){
-            if (exists $studentExam->{$q}{$ans}){
-      				if($studentExam->{$q}{$ans} ne $masterExam->{$q}{$ans}){
-      					$point = 0;
-      				}
-            } else {
-              $examFailureMessage .= "\n\t" ."Missing Answer: " .$ans ."\t in $q";
-            }
-    			}
+    # identify question in student exams
+    if (exists $studentExam->{$q}){
+      $studentQuestion = $q;
+    } else {
+      $examFailureMessage .= "\n\t" ."Missing question: ".$q;
+      my $lowestdist = 10;
+      foreach my $question(keys %{$studentExam}){
+        if (compare_twounevenstrrings($q, $question) < $lowestdist){
+          $studentQuestion = $question;
         }
-
-      } else{
-        $examFailureMessage .= "\n\t" ."Missing question: ".$q;
       }
-      push @studentExamMarks, $point;
-  	}
-
-    # Output message only if a question or answer is missing
-    if ($examFailureMessage =~ tr/\n//) {
-      say $examFailureMessage;
+      if (defined($studentQuestion)) {
+        $examFailureMessage .= "\n\t" ."using this instead: ".$studentQuestion;
+      }
     }
 
-    $AllExamMarks{$studentExamFilePath} = [@studentExamMarks];
+    # identified a question so go thru the answers
+    if (defined($studentQuestion)) {
+      my @masterSolutions;
+      my @studentSolutions;
+
+      foreach my $ans (keys %{$masterExam->{$q}}){
+        my $studentAns;
+        push @masterSolutions, $masterExam->{$q}{$ans};
+
+        if (exists $studentExam->{$q}{$ans}){
+          $studentAns = $ans;
+        } else {
+          $examFailureMessage .= "\n\t" ."Missing Answer: " .$ans ."\t in $q";
+
+          my $lowestdist = 10;
+          foreach my $nearestStudentAns (keys %{$studentExam->{$q}}){
+            if (compare_twounevenstrrings($ans, $nearestStudentAns) < $lowestdist){
+              $studentAns = $nearestStudentAns;
+            }
+          }
+          if (defined($studentAns)) {
+            $examFailureMessage .= "\n\t" ."using this instead: ".$studentAns;
+          }
+        }
+
+        if (defined($studentAns)) {
+          push @studentSolutions, $studentExam->{$studentQuestion}{$studentAns};
+        } else{
+          push @studentSolutions, "-";
+        }
+      }
+
+      # Auswerten
+      if (all { $_ ne 1 } @studentSolutions) {
+        $point = "-";
+      } else {
+        my @bla;
+        foreach my $i (0 .. $#masterSolutions){
+          if ((@masterSolutions[$i] eq @studentSolutions[$i])
+                || (@masterSolutions[$i] eq 0 && @studentSolutions ne 1)
+                || (@masterSolutions[$i] eq 1 && @studentSolutions eq "-")){
+            push @bla, 1;
+          } else {
+            push @bla, 0;
+          }
+        }
+        $point = (all { $_ eq 1 } @bla) ? 1:0;
+      }
+      say @masterSolutions;
+      say @studentSolutions;
+      say "\n"
+    }
+
+    push @studentExamMarks, $point;
   }
+
+  # Output message only if a question or answer is missing
+  if ($examFailureMessage =~ tr/\n//) {
+    say $examFailureMessage. "\n";
+  }
+
+  $AllExamMarks{$studentExamFilePath} = [@studentExamMarks];
+}
 
 
 # Create an prin statistics
@@ -77,7 +126,7 @@ foreach my $key (nsort keys %AllExamMarks){
   my $correctAnswers = 0;
   my $answerd = 0;
 
-  foreach my $answer (@{%AllExamMarks{$key}}){
+  foreach my $answer (@{$AllExamMarks{$key}}){
     if ($answer eq 1) {
       $correctAnswers++;
       $answerd++;
